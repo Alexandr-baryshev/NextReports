c1126f36e743208825a087a712c7cf31288feac9
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-05-13 16:02:58 +1000

    Save

---
+++ src/main/java/mainPackage/MongoConfig.java
@@ -0,0 +1,21 @@
+package mainPackage;
+
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+   @Bean
+   public MongoClient mongoX() {
+      return new MongoClient("localhost");
+   }
+
+   @Bean
+   public MongoTemplate mongoTemplate() throws Exception {
+      return new MongoTemplate(mongoX(), "GeneratorDB");
+   }
+
+}
