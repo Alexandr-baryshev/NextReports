aa63949c853836a6e66dc6283d4c44efe85c5cf7
alexandr_baryshev <alexandr.baryshev@hotmail.com>
2020-05-14 17:14:00 +1000

    Save

--- src/main/java/mainPackage/controllers/HTTPController.java
+++ src/main/java/mainPackage/controllers/HTTPController.java
@@ -8,23 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HTTPController {
 
+//   @GetMapping("/")
+//   public String loginMob(Model model) {
+//      return "login-mob";
+//   }
+//
+//   @GetMapping("/main-mob")
+//   public String mainMob(Model model) {
+//      return "main-mob";
+//   }
+
    @GetMapping("/")
-   public String loginMob(Model model) {
-      return "login-mob";
-   }
-
-   @GetMapping("/main-mob")
-   public String mainMob(Model model) {
-      return "main-mob";
-   }
-
-   @GetMapping("/newReport-mob")
    public String newReportMob(Model model) {
       return "newReport-mob";
    }
 
-   @GetMapping("/reportList-mob")
-   public String reportListMob(Model model) {
-      return "reportList-mob";
-   }
+//   @GetMapping("/reportList-mob")
+//   public String reportListMob(Model model) {
+//      return "reportList-mob";
+//   }
+
 }
